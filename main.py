c106ed6e289877a7678fbf50b6964b6c6ca62bf4
Jesper Fritsch <jesperf96@gmail.com>
2024-04-28 22:49:39 +0200

    prep for snake stream

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from multiprocessing import Pipe, Process, Queue
 
 from snake_sim.snake_env import SnakeEnv
+from snake_sim.render.core import FrameBuilder
 from snake_sim.snakes.autoSnake4 import AutoSnake4
 from snake_sim.default_config import snake_configs
 
@@ -44,20 +45,25 @@ async def websocket_endpoint(websocket: WebSocket):
     # Receive initial configuration data
     try:
         config = await websocket.receive_json()
+        data_mode = config.get('data_mode', 'steps')
         ack = 'ACK'
         await websocket.send_text(ack)
         parent_conn, child_conn = Pipe()
         env_p = Process(target=start_stream_run, args=(child_conn, config))
         env_p.start()
-
+        frame_builder = FrameBuilder()
         while env_p.is_alive():
             # Depending on the config, decide what data to send
             try:
                 step_data = await nonblock_exec(parent_conn.recv)
             except EOFError:
                 break
-            step_json = json.dumps(step_data)
-            await websocket.send_json(step_json)
+            step_dict = json.dumps(step_data)
+            if data_mode == 'steps':
+                payload = step_dict
+            elif data_mode == 'pixel_data':
+                payload = frame_builder.step_to_pixel_changes(step_dict)
+            await websocket.send_json(payload)
     except WebSocketDisconnect as e:
         print(f"Connection closed with error: {e}")
 
