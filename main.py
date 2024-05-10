374d8bf8fd65fb6130bcddc3c3e319dd247817d1
Jesper Fritsch <jesperf96@gmail.com>
2024-05-10 20:14:34 +0200

    Closing process instead of terminate

--- main.py
+++ main.py
@@ -101,5 +101,5 @@ async def websocket_endpoint(websocket: WebSocket):
     finally:
         log.error('Closing connection')
         nr_of_streams -= 1
-        env_p.terminate()
+        env_p.close()
         await websocket.close()
