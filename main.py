73529c59eb0b1122391c0e937afb943e252cc6e2
Jesper Fritsch <jesperf96@gmail.com>
2024-05-10 21:15:15 +0200

    updates

--- main.py
+++ main.py
@@ -104,5 +104,6 @@ async def websocket_endpoint(websocket: WebSocket):
         log.info('Cleaning up...')
         nr_of_streams -= 1
         env_p.terminate()
+        env_p.join()
         if websocket.state != WebSocketState.DISCONNECTED:
             await websocket.close()
