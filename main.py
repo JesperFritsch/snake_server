6ad6271f92cd3711667550f7a21e2710e32e7e75
Jesper Fritsch <jesperf96@gmail.com>
2024-08-08 11:18:13 +0200

    Implementing data on demand

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from logging.handlers import RotatingFileHandler
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.websockets import WebSocketState
 from multiprocessing import Pipe, Process, Queue, get_context
+from collections import deque
 
 from snake_sim.snake_env import SnakeEnv
 from snake_sim.render.core import FrameBuilder
@@ -37,8 +38,40 @@ with open(config_json, 'r') as f:
     snake_defalut_config = json.load(f)
 
 
+class DataOnDemand:
+    def __init__(self, websocket: WebSocket, data_mode: str, on_demand: bool):
+        self.data_mode = data_mode
+        self.websocket = websocket
+        self.on_demand = on_demand
+        self.data_end = False
+        self.yield_time = 0.01
+        self.data_buffer = deque()
+
+    def push_data(self, data):
+        self.data_buffer.append(data)
+
+    async def handler(self):
+        while not self.data_end or len(self.data_buffer) > 0:
+            if self.on_demand:
+                req = await self.websocket.receive_text()
+                get, nr = req.split(' ')
+                nr_changes = int(nr)
+            else:
+                nr_changes = 1
+                await asyncio.sleep(self.yield_time)
+            count = 0
+            while count < nr_changes:
+                count += 1
+                if len(self.data_buffer) > 0:
+                    data = self.data_buffer.popleft()
+                    if self.data_mode == 'steps':
+                        await self.websocket.send_json(data)
+                    elif self.data_mode == 'pixel_data':
+                        await self.websocket.send_bytes(data)
+
 async def nonblock_exec(func, *args):
     return await asyncio.to_thread(func, *args)
+
 
 def start_stream_run(conn, config):
     sys.stdout = open(os.devnull, 'w')
@@ -68,10 +101,11 @@ async def websocket_endpoint(websocket: WebSocket):
         log.info(f'Accepted connection nr: {nr_of_streams}')
     else:
         return
-    # Receive initial configuration data
+        # Receive initial configuration data
     try:
         config = await websocket.receive_json()
         data_mode = config.get('data_mode', 'steps')
+        data_on_demand = config.get('data_on_demand', False)
         ack = 'ACK'
         log.info(f'sending {ack} to client')
         await websocket.send_text(ack)
@@ -84,24 +118,27 @@ async def websocket_endpoint(websocket: WebSocket):
         await websocket.send_json(init_data)
         if data_mode == 'pixel_data':
             frame_builder = FrameBuilder(run_meta_data=init_data, expand_factor=2, offset=(1, 1))
+        dod = DataOnDemand(websocket, data_mode, data_on_demand)
+        dod_task = asyncio.create_task(dod.handler())
         log.info(f'Sending data with mode: {data_mode}')
         while env_p.is_alive():
             # Depending on the config, decide what data to send
             if parent_conn.poll(timeout=0.1):
                 try:
                     step_data = await nonblock_exec(parent_conn.recv)
+                    if data_mode == 'steps':
+                        payload = step_data
+                    elif data_mode == 'pixel_data':
+                        changes = frame_builder.step_to_pixel_changes(step_data)
+                        for change in changes:
+                            flattened_payload = [value for sublist in change for sublist_pair in sublist for value in sublist_pair]
+                            payload = bytes(flattened_payload)
+                    dod.push_data(payload)
                 except EOFError:
                     break
-                if data_mode == 'steps':
-                    payload = step_data
-                    await websocket.send_json(payload)
-                elif data_mode == 'pixel_data':
-                    changes = frame_builder.step_to_pixel_changes(step_data)
-                    for change in changes:
-                        flattened_payload = [value for sublist in change for sublist_pair in sublist for value in sublist_pair]
-                        payload = bytes(flattened_payload)
-                        await websocket.send_bytes(payload)
-        await websocket.send_text('END')
+        dod.data_end = True
+        await dod_task
+
     except WebSocketDisconnect as e:
         log.info(f"Connection closed")
 
