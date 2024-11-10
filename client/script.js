547cda9dc18f5c3737c64a3278ac963d92bd6a9b
Jesper Fritsch <jesper.fritsch@infotiv.se>
2024-11-10 20:38:21 +0100

    Improving the webinterface

--- client/script.js
+++ client/script.js
@@ -1,13 +1,12 @@
 
-const base_uri = "ws://localhost:42069/stream";
-const request_uri = "http://localhost:42069/api/request_run";
+const domain = `localhost:42069`;
 
 const basic_config = {
-    grid_width: 32,
-    grid_height: 32,
+    grid_width: 64,
+    grid_height: 64,
     food: 50,
     snake_count: 1,
-    map: "items"
+    map: "sign"
 };
 
 function blobToArrayBuffer(blob) {
@@ -29,8 +28,8 @@ class FrameHandler {
         this.pad_y = null;
         this.total_width = null;
         this.total_height = null;
-        this.frames = [];
-        this.last_frame = null;
+        this.frame_images = [];
+        this.last_frame_data = null;
     }
 
     /**
@@ -39,7 +38,7 @@ class FrameHandler {
      */
     set_base_map(base_map) {
         console.log(base_map);
-        this.last_frame = base_map.slice();
+        this.last_frame_data = base_map.slice();
     }
 
     init(width, height, content_offset_x, content_offset_y, pad_x, pad_y) {
@@ -54,8 +53,8 @@ class FrameHandler {
     }
 
     clear() {
-        this.frames = [];
-        this.last_frame = null;
+        this.frame_images = [];
+        this.last_frame_data = null;
         this.width = null;
         this.height = null;
         this.content_offset_x = null;
@@ -67,19 +66,18 @@ class FrameHandler {
     }
 
     add_frame(pixel_changes_data) {
-        const frame = new Uint8ClampedArray(this.last_frame);
+        const frame = new Uint8ClampedArray(this.last_frame_data);
         for (const pixel_data of pixel_changes_data) {
             const coord = pixel_data[0];
             const color = pixel_data[1];
             const index = (coord[0] + (coord[1] * this.total_width)) * 4;
-            console.log(coord, color, index);
             for (let i = 0; i < 4; i++) {
                 frame[index + i] = color[i];
             }
             frame[index + 3] = 255;
         }
-        this.frames.push(frame);
-        this.last_frame = frame.slice();
+        this.frame_images.push(frame);
+        this.last_frame_data = frame.slice();
     }
 
 }
@@ -92,9 +90,8 @@ class CanvasHandler {
         this.canvas.width = 800;
         this.canvas.height = 800;
         this.frame_handler = new FrameHandler();
-        this.stream_handler = new StreamHandler();
-        this.stream_handler.on_init_data = this.init.bind(this);
-        this.stream_handler.set_msg_handler(this.frame_handler.add_frame.bind(this.frame_handler));
+        this.stream_handler = null;
+        this.reset_stream_handler(domain); // creates a new stream handler
         this.frame_index = 0;
         this.frame_interval = 1 / 20; // 20 fps
         this.interval_id = null;
@@ -105,8 +102,22 @@ class CanvasHandler {
         this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
     }
 
+    reset_stream_handler(host_domain) {
+        if (this.stream_handler) {
+            this.stream_handler.reset();
+        }
+        this.stream_handler = new StreamHandler(host_domain);
+        this.stream_handler.on_init_data = this.init.bind(this);
+        this.stream_handler.set_msg_handler(this.frame_handler.add_frame.bind(this.frame_handler));
+    }
+
+    start_stream(stream_id) {
+        this.reset_stream_handler(domain);
+        this.stream_handler.join_stream(stream_id);
+        this.run();
+    }
+
     init(init_data){
-        console.log(init_data);
         this.frame_handler.init(init_data.width * 2, init_data.height * 2, 1, 1, 0, 0);
         this.canvas.width = this.frame_handler.total_width;
         this.canvas.height = this.frame_handler.total_height;
@@ -160,28 +171,29 @@ class CanvasHandler {
         this.frame_handler.set_base_map(base_map_frame);
     }
 
-    show_frame(frame) {
-        const img = new ImageData(frame, this.frame_handler.total_width);
-        this.context.putImageData(img, 0, 0);
+    show_frame(frame_img) {
+        const img_data = new ImageData(frame_img, this.frame_handler.total_width);
+        this.context.putImageData(img_data, 0, 0);
     }
 
     run() {
         this.interval_id = setInterval(() => {
-            if (this.frame_index < this.frame_handler.frames.length) {
-                this.show_frame(this.frame_handler.frames[this.frame_index]);
+            if (this.frame_index < this.frame_handler.frame_images.length) {
+                this.show_frame(this.frame_handler.frame_images[this.frame_index]);
+                this.frame_index++;
             }
-            this.frame_index++;
         }, 100);
     }
 
     stop() {
+        this.frame_index = 0;
         clearInterval(this.interval_id);
     }
 
 }
 
 class StreamHandler {
-    constructor() {
+    constructor(host_domain) {
         this.msg_handler = null;
         this.data_mode = "pixel_data";
         this.data_on_demain = false;
@@ -189,6 +201,7 @@ class StreamHandler {
         this.on_init_data = null; // callback
         this.got_init_data = false;
         this.ws = null;
+        this.host_domain = host_domain;
     }
 
     reset() {
@@ -200,7 +213,9 @@ class StreamHandler {
     }
 
     async request_run(config){
-        return fetch(request_uri, {
+        const request_url = `http://${this.host_domain}/api/request_run`
+
+        return fetch(request_url, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -216,11 +231,13 @@ class StreamHandler {
     }
 
     join_stream(stream_id){
-        const url = new URL(base_uri + "/" + stream_id);
+        const stream_url = `http://${this.host_domain}/stream/${stream_id}`; 
+        const url = new URL(stream_url);
         url.searchParams.append("data_on_demand", this.data_on_demain);
         url.searchParams.append("data_mode", this.data_mode);
         this.ws = new WebSocket(url.toString());
         this.ws.onmessage = this.msg_reciever.bind(this);
+        this.ws.binaryType = "arraybuffer";
     }
 
     msg_reciever(message) {
@@ -231,16 +248,15 @@ class StreamHandler {
         }
         else {
             // console.log(message.data);
-            blobToArrayBuffer(message.data).then(data => {
-                const view = new DataView(data);
-                const pixels = [];
-                for (let i = 0; i < view.byteLength; i += 5) {
-                    const coord = [view.getUint8(i), view.getUint8(i + 1)];
-                    const color = [view.getUint8(i + 2), view.getUint8(i + 3), view.getUint8(i + 4)];
-                    pixels.push([coord, color]);
-                }
-                this.msg_handler(pixels);
-            });
+            const data = message.data;
+            const view = new DataView(data);
+            const pixels = [];
+            for (let i = 0; i < view.byteLength; i += 5) {
+                const coord = [view.getUint8(i), view.getUint8(i + 1)];
+                const color = [view.getUint8(i + 2), view.getUint8(i + 3), view.getUint8(i + 4)];
+                pixels.push([coord, color]);
+            }
+            this.msg_handler(pixels);
         }
     }
 
@@ -253,10 +269,54 @@ class StreamHandler {
     }
 }
 
-const canvas_handler = new CanvasHandler();
-const stream_handler = canvas_handler.stream_handler;
-stream_handler.request_run(basic_config).then(stream_id => {
+
+function populate_stream_list(streams) {
+    const stream_list = document.getElementById('activeStreamsList');
+    stream_list.innerHTML = '';
+    for (const stream of streams) {
+        const stream_item = document.createElement('li');
+        stream_item.appendChild(document.createTextNode(stream));
+        stream_item.onclick = start_stream_event;
+        stream_list.appendChild(stream_item);
+    }
+}
+
+async function get_active_streams() {
+    const url = `http://${domain}/api/run_info`;
+    return await fetch(url).then(response => {
+        return response.json();
+    }).then(data => {
+        const stream_ids = Object.keys(data);
+        populate_stream_list(stream_ids);
+        return data;
+    }).catch(error => {
+        console.error('Error:', error);
+    });
+
+}
+
+async function start_stream_event(event) {
+    canvas_handler.stop();
+    stream_id = event.target.innerText;
     console.log(stream_id);
-    stream_handler.join_stream(stream_id);
+    const run_info = await get_active_streams();
+    console.log(run_info);
+    const current_step = run_info[stream_id].steps;
+    canvas_handler.frame_index = current_step * 2;
+    canvas_handler.frame_handler.clear();
+    canvas_handler.reset_stream_handler(domain);
+    canvas_handler.stream_handler.join_stream(stream_id);
     canvas_handler.run();
+}
+
+
+const canvas_handler = new CanvasHandler();
+document.addEventListener('DOMContentLoaded', () =>{
+    get_active_streams();
 });
+document.getElementById('requestButton').onclick = () => {
+    const stream_handler = canvas_handler.stream_handler;
+    stream_handler.request_run(basic_config).then(stream_id => {
+        get_active_streams();
+    });
+};
