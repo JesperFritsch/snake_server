d57a18c88e960515a677d0efeb4409d74eca71da
Jesper Fritsch <jesperf96@gmail.com>
2025-01-21 22:20:35 +0100

    Change start script

--- start_server.sh
+++ start_server.sh
@@ -6,20 +6,14 @@ RELOAD=""
 # Parse arguments
 while [[ "$#" -gt 0 ]]; do
     case $1 in
-        -d|--dev) RELOAD="--reload"; shift ;;
+        -d|--dev) DEV="--dev"; shift ;;
         *) echo "Unknown parameter passed: $1"; exit 1 ;;
     esac
     shift
 done
 
-if [[ "$(uname -s)" == *"CYGWIN"* || "$(uname -s)" == *"MINGW"* || "$(uname -s)" == *"MSYS"* ]]; then
-  # Windows path
-  source /b/pythonStuff/snake_server/server_venv/Scripts/activate
-else
-  # Unix-like system path
-  source /home/jesper/py_fun/snake_server/server_venv/bin/activate
-fi
+source /home/jesper/py_fun/snake_server/server_venv/bin/activate
 
 umask 000
-uvicorn main:app --host 0.0.0.0 --port 42069 $RELOAD
+run-snake-server $DEV
 deactivate
